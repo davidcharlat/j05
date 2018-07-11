3309524569cd1b6e1702f46dc942ee24e7c56ea9
davidcharlat <39385280+davidcharlat@users.noreply.github.com>
2018-07-11 21:56:29 +0200

    Update ft_strupcase.c

--- ex05/ft_strupcase.c
+++ ex05/ft_strupcase.c
@@ -1,9 +1,14 @@
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+char	*ft_strupcase(char *str)	
 {
 	int cursor;
 
 	cursor = 0;
-	while (*(s1 + cursor) != '\0' && *(s1 + cursor) == *(s2 + cursor) && cursor <= n)
-			cursor++;
-	return ((char)*(s1+cursor)-(char)*(s2+cursor));
+	while (str[cursor] != '\0')
+	{
+		if ((str[cursor] < 123) && (str[cursor] > 96))
+			str[cursor] -= 32;
+		cursor++;
+	}
+
+	return (str);
 }
